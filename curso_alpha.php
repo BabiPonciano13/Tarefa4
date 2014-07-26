013b95dcbf462db8db3cb72dd273a8077e28c509
Bárbara Ponciano <barbaraponciano13@gmail.com>
2014-07-25 23:46:33 -0400

    [ADJUST] Modificando pagina inicial.
    Signed-off-by: Bárbara Ponciano <barbaraponciano13@gmail.com>

--- curso_alpha.php
+++ curso_alpha.php
@@ -1,6 +1,28 @@
 <?php
 include 'header.php';
 include 'body.php';
+ ?>
+
+<div style="text-align: center">
+    <h1>Curso Alpha</h1> 
+    <br>
+    <p>
+
+Vivamus ullamcorper rhoncus purus blandit mollis. Suspendisse eget purus at mi varius ultrices nec volutpat tortor. Etiam sed condimentum elit. Aliquam aliquet iaculis erat ut luctus. Fusce sit amet lacinia nunc. Vivamus felis neque, lobortis sit amet libero vel, molestie vulputate sem. Sed feugiat turpis et nisl mollis aliquam. Nunc venenatis turpis a odio pulvinar, vitae dapibus tellus faucibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam pharetra ut libero et aliquet. Phasellus id erat dignissim, luctus odio quis, bibendum mauris. Sed ultrices arcu in dignissim iaculis. Maecenas sed risus condimentum, tincidunt nunc nec, consequat diam. Sed congue molestie metus, porttitor porta magna viverra et. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+    </p>
+    <p>
+Curabitur vitae lobortis nisi. Suspendisse molestie ut massa id aliquet. Sed hendrerit tincidunt enim, non lacinia turpis ullamcorper sed. Nunc est orci, tincidunt et hendrerit vel, tempor eu erat. Mauris mi enim, tincidunt eu pharetra venenatis, consequat id augue. Nam dignissim sodales turpis quis adipiscing. Suspendisse tempus, sapien sit amet convallis adipiscing, eros arcu ultrices felis, non pulvinar ante velit vel tortor. Phasellus a sapien non risus dapibus auctor sed ut mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam eu erat purus. Curabitur sed enim bibendum, ultrices quam malesuada, blandit mauris.
+    </p>
+    <p>
+Vestibulum leo neque, blandit a quam ut, blandit tempor tortor. Etiam dictum tortor non felis dictum, in porttitor risus venenatis. Suspendisse id hendrerit lectus. Proin at pulvinar nunc. Etiam vitae commodo sem, eu ultrices felis. Fusce quam magna, consectetur vitae venenatis vel, facilisis quis libero. Donec vel viverra purus, ac consectetur magna. Mauris et pretium lorem, et mollis elit.
+    </p>
+    <p>
+Donec eget imperdiet diam. Cras sit amet blandit odio. Suspendisse varius pharetra semper. Mauris cursus urna in metus scelerisque porttitor. Nullam tempor tincidunt velit non ullamcorper. Maecenas quis orci pretium, porttitor diam a, aliquet tellus. Sed a bibendum dui, aliquam mollis est. Mauris mollis enim justo, vitae faucibus neque pulvinar vel. Suspendisse potenti. Nullam tristique dictum consectetur. Mauris erat purus, volutpat eget nisi eu, iaculis ultricies ligula. Aenean sodales elit vel libero lobortis convallis. Sed ut est metus. Phasellus ultrices risus eget faucibus cursus.
+    </p>
+    <p>
+Sed congue diam eget imperdiet posuere. In cursus non ipsum vel faucibus. Aenean ullamcorper placerat urna et imperdiet. Duis dignissim augue tincidunt, commodo quam in, vestibulum leo. Pellentesque vel iaculis lorem, ac consectetur augue. Praesent vitae facilisis magna. Etiam tincidunt sit amet mauris at luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse ornare sem ut mauris auctor aliquam. Proin eleifend rhoncus orci rutrum commodo. 
+</p>
+</div>
+
+<?php
 include 'footer.php';
-
-
